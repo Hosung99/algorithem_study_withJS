d9b23e63babf9cacceb9970ec74ca15e2cc7490c
함지현 <gkawlgus01@naver.com>
2024-07-06 12:44:54 +0900

    fix: 백준 제출 코드로 변경

--- hamjihyeon/week03/2805.js
+++ hamjihyeon/week03/2805.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
-// const input = fs.readFileSync("/dev/stdin").toString().trim().split('\n');
-const input = fs.readFileSync("hamjihyeon/week03/test.txt").toString().trim().split('\n');
+const input = fs.readFileSync("/dev/stdin").toString().trim().split('\n');
 
 function cutTree() {
   const [N, M] = input[0].split(' ').map(Number);
