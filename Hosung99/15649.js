a337d7b7db667331d588c43f64d426d06a21e0fe
Sungho <shson1217@kw.ac.kr>
2024-07-11 15:16:39 +0900

    refactor: N과 M (1)

--- Hosung99/15649.js
+++ Hosung99/15649.js
@@ -1,29 +1,29 @@
 const fs = require("fs");
 
 const input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
-let [N, M] = [+input[0], +input[1]];
-
-let visited = new Array(9).fill(false);
-let arr = [];
-
-function dfs(depth) {
-  if (depth === M) {
-    console.log(arr.join(" "));
-    return;
-  }
-  for (let i = 1; i <= N; i++) {
-    if (visited[i] === false) {
-      arr.push(i);
-      visited[i] = true;
-      dfs(depth + 1);
-	  arr.pop();
-	  visited[i] = false;
-    }
-  }
-}
 
 function solution() {
+  let [N, M] = [+input[0], +input[1]];
+  let visited = new Array(9).fill(false);
+  let arr = [];
+  let ans = "";
+  function dfs(depth) {
+    if (depth === M) {
+      ans += arr.join(" ") + "\n";
+      return;
+    }
+    for (let i = 1; i <= N; i++) {
+      if (visited[i] === false) {
+        arr.push(i);
+        visited[i] = true;
+        dfs(depth + 1);
+        arr.pop();
+        visited[i] = false;
+      }
+    }
+  }
   dfs(0);
+  return ans;
 }
 
-solution();
+console.log(solution());
